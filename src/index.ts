a83e7ee4a421aa48ffaf65288f6a9de006919727
Pietro Verdile <pietro.verdile@visto.global>
2024-10-25 14:34:44 -0300

    fix linter mess

--- src/index.ts
+++ src/index.ts
@@ -1,62 +1,62 @@
-import type { IncomingMessage, ServerResponse } from "http";
-import type { AuthConfig, Session } from "@auth/core/types";
-import type { FastifyPluginCallback, FastifyRequest } from "fastify";
-import fastifyPlugin from "fastify-plugin";
-import { createAuthMiddleware, getSession } from "authey";
-import Middie from "./middie";
+import type { IncomingMessage, ServerResponse } from 'http'
+import type { AuthConfig, Session } from '@auth/core/types'
+import type { FastifyPluginCallback, FastifyRequest } from 'fastify'
+import fastifyPlugin from 'fastify-plugin'
+import { createAuthMiddleware, getSession } from 'authey'
+import Middie from './middie'
 
-const plugin: FastifyPluginCallback<AuthConfig> = (fastify, options, done) => {
-  const middleware = createAuthMiddleware(options);
+const plugin: FastifyPluginCallback<AuthConfig> = (
+  fastify,
+  options,
+  done,
+) => {
+  const middleware = createAuthMiddleware(options)
 
-  const middie = Middie(
-    (
-      err: Error,
-      _req: IncomingMessage,
-      _res: ServerResponse,
-      next: (err?: Error) => void
-    ) => {
-      next(err);
-    }
-  );
+  const middie = Middie((err: Error, _req: IncomingMessage, _res: ServerResponse, next: (err?: Error) => void) => {
+    next(err)
+  })
 
-  middie.use(middleware);
+  middie.use(middleware)
 
   function runMiddie(req: any, reply: any, next: (err?: Error) => void) {
-    req.raw.originalUrl = req.raw.url;
-    req.raw.id = req.id;
-    req.raw.hostname = req.hostname;
-    req.raw.ip = req.ip;
-    req.raw.ips = req.ips;
-    req.raw.log = req.log;
-    req.raw.body = req.body;
-    req.raw.query = req.query;
-    reply.raw.log = req.log;
+    req.raw.originalUrl = req.raw.url
+    req.raw.id = req.id
+    req.raw.hostname = req.hostname
+    req.raw.ip = req.ip
+    req.raw.ips = req.ips
+    req.raw.log = req.log
+    req.raw.body = req.body
+    req.raw.query = req.query
+    reply.raw.log = req.log
     for (const [key, val] of Object.entries(reply.getHeaders())) {
-      reply.raw.setHeader(key, val);
+      reply.raw.setHeader(key, val)
     }
-    middie.run(req.raw, reply.raw, next);
+    middie.run(req.raw, reply.raw, next)
   }
 
-  fastify.addHook("onRequest", runMiddie);
+  fastify.addHook('onRequest', runMiddie)
   // eslint-disable-next-line prefer-arrow-callback
-  fastify.decorate("getSession", function (req: FastifyRequest) {
-    return getSession(req.raw, options);
-  });
+  fastify.decorate('getSession', function (req: FastifyRequest) {
+    return getSession(req.raw, options)
+  })
 
-  done();
-};
+  done()
+}
 
 const fastifyNextAuth = fastifyPlugin(plugin, {
-  fastify: "5.x",
-  name: "fastify-next-auth",
-});
+  fastify: '5.x',
+  name: 'fastify-next-auth',
+})
 
-export { fastifyNextAuth, AuthConfig };
+export {
+  fastifyNextAuth,
+  AuthConfig,
+}
 
-export default fastifyNextAuth;
+export default fastifyNextAuth
 
-declare module "fastify" {
+declare module 'fastify' {
   interface FastifyInstance {
-    getSession(req: FastifyRequest): Session;
+    getSession(req: FastifyRequest): Session
   }
 }
